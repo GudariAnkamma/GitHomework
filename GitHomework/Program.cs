d8429a2f3a33ab3ad9230ee70569d9f42a11922d
GudariAnkamma <GUAN@simcorp.com>
2026-08-14 10:40:03 +0530

    Console was changed

--- GitHomework/Program.cs
+++ GitHomework/Program.cs
@@ -10,7 +10,7 @@ namespace GitHomework
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello world!");
         }
     }
 }
