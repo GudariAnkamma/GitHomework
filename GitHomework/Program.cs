47566a59f3bff1e9a089bde9e4027a59c285e5b5
GudariAnkamma <GUAN@simcorp.com>
2026-08-17 19:00:14 +0530

    current value calculations

--- GitHomework/Program.cs
+++ GitHomework/Program.cs
@@ -6,11 +6,26 @@ using System.Threading.Tasks;
 
 namespace GitHomework
 {
-    internal class Program
+    enum TransactionType
+    {
+        Buy,
+        Sell
+    }
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            Console.WriteLine("Input the nominal of the trade:");
+            string userInput = Console.ReadLine();
+            int nominal = Int32.Parse(userInput);
+
+            Console.WriteLine("Input the price of the trade:");
+            userInput = Console.ReadLine();
+            decimal price = Decimal.Parse(userInput);
+
+            Console.WriteLine("Input the transaction type (Buy/Sell):");
+            userInput = Console.ReadLine();
+            TransactionType trcType = (TransactionType)Enum.Parse(typeof(TransactionType), userInput, true);
         }
     }
 }
