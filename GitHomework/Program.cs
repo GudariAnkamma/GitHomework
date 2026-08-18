466da727659e591384198e1b1b40ac68d6fcec2f
GudariAnkamma <GUAN@simcorp.com>
2026-08-18 20:21:19 +0530

    commit changes

--- GitHomework/Program.cs
+++ GitHomework/Program.cs
@@ -10,7 +10,21 @@ namespace GitHomework
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            Console.WriteLine("Input the nominal of the trade:");
+            string userInput = Console.ReadLine();
+            int nominal = Int32.Parse(userInput);
+
+            Console.WriteLine("Input the trade price:");
+            userInput = Console.ReadLine();
+            double tradePrice = Double.Parse(userInput);
+
+            Console.WriteLine("Input transaction type (Buy/Sell):");
+            userInput = Console.ReadLine();
+            TransactionType transactionType = (TransactionType)Enum.Parse(typeof(TransactionType), userInput, true);
+
+            int factor = transactionType == TransactionType.Buy ? 1 : -1;
+            double currentValue = nominal * tradePrice * factor;
+            Console.WriteLine($"Current Value: {currentValue}");
         }
     }
 }
